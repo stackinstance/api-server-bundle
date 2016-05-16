4d213060640c095d6ee825689d913fadbdfb7b92
Ray Kootstra <r.kootstra@kootstradevelopment.nl>
2016-05-16 08:25:25 +0200

    created add RESTful

--- Controller/InstanceController.php
+++ Controller/InstanceController.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace StackInstance\ApiServerBundle\Controller;
+use Doctrine\DBAL\DBALException;
 use StackInstance\ApiServerBundle\Entity\Instance;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * InstanceController
@@ -30,15 +32,31 @@ class InstanceController extends AbstractApiController
     }
 
     /**
+     * @param Request $request
      * @return \Symfony\Component\HttpFoundation\JsonResponse
      */
-    public function addAction()
+    public function addAction(Request $request)
     {
         if ($this->isAuthorized() !== true) {
             return $this->errorResponse(401, 'Unauthorized');
         }
 
-        $responseData = ['data' => 'test'];
+        if ($this->isValidAddCall($request) === false) {
+            return $this->errorResponse(401, 'No title provided');
+        }
+
+        /** @var Instance $instance */
+        $instance = $this->createInstance($request);
+
+        if ($instance === false) {
+            return $this->errorResponse(401, 'Could not create instance');
+        }
+
+        $response = ['id' => $instance->getId(),
+                     'title' => $instance->getTitle()
+        ];
+
+        $responseData = ['data' => $response];
         return $this->successResponse($responseData);
     }
 
@@ -63,8 +81,43 @@ class InstanceController extends AbstractApiController
         if ($this->isAuthorized() !== true) {
             return $this->errorResponse(401, 'Unauthorized');
         }
-        
+
         $responseData = ['data' => 'test'];
         return $this->successResponse($responseData);
+    }
+
+    /**
+     * @param Request $request
+     * @return bool
+     */
+    protected function isValidAddCall(Request $request)
+    {
+        $title = $request->get('title');
+        if ($title ===  null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param Request $request
+     * @return bool|Instance
+     */
+    protected function createInstance(Request $request)
+    {
+        $title = $request->get('title');
+
+        $instance = new Instance();
+        $instance->setTitle($title);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        try {
+            $entityManager->persist($instance);
+            $entityManager->flush();
+        } catch (DBALException $e) {
+            return false;
+        }
+        return $instance;
     }
 }
